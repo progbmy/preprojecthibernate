05c9584f01c4bea7c76a106a8e71e6ce340b53e7
MIhail <skuter63@mail.ru>
2021-11-27 20:41:52 +0400

    correct main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,14 +8,13 @@ public class Main {
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
-          userService.dropUsersTable();
-//        userService.saveUser("Mike", "Dzen", (byte) 12);
-//        userService.saveUser("Petr", "Ozerov", (byte) 18);
-//        userService.saveUser("Zina", "kelli", (byte) 45);
-//        userService.saveUser("Oracle", "Data", (byte) 50);
-//        userService.getAllUsers();
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+        userService.saveUser("Mike", "Dzen", (byte) 12);
+        userService.saveUser("Petr", "Ozerov", (byte) 18);
+        userService.saveUser("Zina", "kelli", (byte) 45);
+        userService.saveUser("Oracle", "Data", (byte) 50);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
